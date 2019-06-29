4057a9f577f060d34825560f86d4d6d7cb3b8a2e
web-ali <web-ali@yandex.ru>
2019-06-29 15:22:54 +0000

    add abstract mapper

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
-$pdo = new PDO("mysql:host=mysql;dbname=orm","root","12345");
-$r = $pdo->query("Select * from user")->fetchAll(PDO::FETCH_ASSOC);
 
-print_r($r);
+require "../vendor/autoload.php";
+
+// $pdo = new PDO("mysql:host=mysql;dbname=orm","root","12345");
+// $r = $pdo->query("Select * from user")->fetchAll(PDO::FETCH_ASSOC);
+
+// print_r($r);
+
 ?>
